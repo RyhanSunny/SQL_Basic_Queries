fb477e8516612e84ce3c33c60a9e6bf4a9065007
Ryhan Sunny <sunnyRayhan2@gmail.com>
2019-09-05 22:51:03 +0200

    Update TheWorld.sql

--- TheWorld.sql
+++ TheWorld.sql
@@ -23,3 +23,40 @@ WHERE area > 3000000 OR population > 250000000
 --Exclusive OR (XOR). Show the countries that are big by area or big by population but not both. Show name, population and area.
 SELECT name, population, area FROM world 
 WHERE area > 3000000 XOR population > 250000000
+
+--Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+--For South America show population in millions and GDP in billions both to 2 decimal places.
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world 
+WHERE continent LIKE 'South America'
+
+--Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+--Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+SELECT name, ROUND(gdp/population, -3) FROM world
+WHERE gdp > 1000000000000
+
+--Greece has capital Athens.
+--Each of the strings 'Greece', and 'Athens' has 6 characters.
+--Show the name and capital where the name and the capital have the same number of characters.
+--You can use the LENGTH function to find the number of characters in a string
+SELECT name, capital
+  FROM world
+ WHERE LENGTH(name) = LENGTH(capital)
+
+--The capital of Sweden is Stockholm. Both words start with the letter 'S'.
+--Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+--You can use the function LEFT to isolate the first character.
+--You can use <> as the NOT EQUALS operator.
+SELECT name, capital FROM world 
+WHERE LEFT(name, 1) LIKE LEFT(capital, 1) AND name <> capital
+
+--Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. They don't count because they have more than one word in the name.
+--Find the country that has all the vowels and no spaces in its name.
+--You can use the phrase name NOT LIKE '%a%' to exclude characters from your results.
+SELECT name
+   FROM world
+WHERE name LIKE '%a%'
+  AND name LIKE '%e%'
+  AND name LIKE '%i%'
+  AND name LIKE '%o%'
+  AND name LIKE '%u%'
+  AND name NOT LIKE '% %'
