9999e67eac24c19c76a84c5af15b8aec9245c415
Ryhan Sunny <sunnyRayhan2@gmail.com>
2019-09-06 16:34:22 +0200

    Update Nobel_Prize_Winners.sql

--- Nobel_Prize_Winners.sql
+++ Nobel_Prize_Winners.sql
@@ -93,3 +93,10 @@ SELECT winner, yr, subject FROM nobel
 WHERE winner LIKE 'sir%'
 ORDER BY yr DESC, winner
 
+--14.
+/*The expression subject IN ('Chemistry','Physics') can be used as a value - it will be 0 or 1.
+Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+*/
+SELECT winner, subject FROM nobel
+ WHERE yr = 1984
+ ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner
