4f07699cdcd1f438129adbcb9bea5ee4d52423aa
Ryhan Sunny <sunnyRayhan2@gmail.com>
2019-09-06 16:22:31 +0200

    Update Nobel_Prize_Winners.sql

--- Nobel_Prize_Winners.sql
+++ Nobel_Prize_Winners.sql
@@ -77,3 +77,11 @@ OR (subject LIKE 'Literature' AND yr >= 2004)
 SELECT * FROM nobel 
 WHERE winner LIKE 'PETER GRÜNBERG'
 
+--12.
+/* Find all details of the prize won by EUGENE O'NEILL
+Escaping single quotes
+You can't put a single quote in a quote string directly. You can use two single quotes within a quoted string.
+*/
+SELECT * FROM nobel 
+WHERE winner LIKE 'EUGENE O''NEILL'
+
