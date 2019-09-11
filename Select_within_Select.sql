d1722c17cf624308697fafe96cc56795013c8278
Ryhan Sunny <sunnyRayhan2@gmail.com>
2019-09-11 21:40:37 +0200

    Update Select_within_Select.sql

--- Select_within_Select.sql
+++ Select_within_Select.sql
@@ -14,7 +14,17 @@ SELECT name FROM world
 per capita gdp: gdp/population
 */
 SELECT name FROM world
-WHERE continent = 'Europe' 
-AND gdp/population > (SELECT gdp/population FROM world 
-                      WHERE name = 'United Kingdom')
+  WHERE continent = 'Europe' 
+    AND gdp/population > (SELECT gdp/population FROM world 
+                          WHERE name = 'United Kingdom')
+
+--#3
+/* List the name and continent of countries in the continents containing 
+either Argentina or Australia. Order by name of the country.
+*/
+SELECT name, continent FROM world
+  WHERE continent IN (SELECT continent FROM world
+                      WHERE name IN ('Argentina', 'Australia'))
+                  ORDER BY name
+
 
